9b0bc49b0316d0c3e8112b753577198c5d1a53ff
Amin Roslan <amnrsln@gmail.com>
2019-02-25 22:11:29 +0800

    fixing exporter

--- index.js
+++ index.js
@@ -1,26 +1,83 @@
 const request = require("request");
 const fs = require("fs");
+const unzip = require("unzipper");
+const zlib = require("zlib");
+class Scraper {
+  constructor() {}
 
-// Your Amplitude API Key
-const user = process.env.AMPLITUDE_API_KEY;
-const pass = process.env.AMPLITUDE_SECRET_KEY;
-
-// The period you want to extract the data from.
-// TODO: remove hardcoded values before deployment
-const startDate = "20190101T00";
-const endDate = "20190102T00";
-
-// Create authentication token
-const auth = new Buffer(user + ":" + pass).toString("base64");
-
-const options = {
-  method: "GET",
-  url: `https://amplitude.com/api/2/export`,
-  qs: { start: startDate, end: endDate },
-  headers: {
-    Authorization: "Basic " + auth
+  login({ user, password }) {
+    if (!user || !password) {
+      console.log("Please complete the missing parameter.");
+    } else if (!user && !password) {
+      console.log("Please provide the API key & Secret key to proceed!");
+    } else {
+      this.user = user;
+      this.password = password;
+    }
   }
-};
 
-// Request the data and pipes the file into the directory
-request(options).pipe(fs.createWriteStream("file.zip"));
+  extractData(start, end, callback) {
+    if (!start || !end) {
+      console.log("Please complete the parameter");
+    } else if (!start && !end) {
+      console.log("Please enter the period of start and end date!");
+    } else {
+      console.log(`Period: ${start} to ${end}\nStarting to look for data.`);
+      const auth = new Buffer(this.user + ":" + this.password).toString(
+        "base64"
+      );
+      const options = {
+        method: "GET",
+        url: `https://amplitude.com/api/2/export`,
+        qs: { start, end },
+        headers: {
+          Authorization: "Basic " + auth
+        }
+      };
+      request(options)
+        .pipe(unzip.Parse())
+        .on("entry", function(entry) {
+          let buffer = "";
+          const gunzip = zlib.createGunzip();
+          entry.pipe(gunzip);
+          gunzip
+            .on("data", data => {
+              buffer += data.toString();
+            })
+            .on("end", () => {
+              // console.log(entry);
+              const improvisedBuffer = JSON.stringify(
+                buffer
+                  .split("\n")
+                  .map(json => {
+                    try {
+                      return JSON.parse(json);
+                    } catch (err) {
+                      return null;
+                    }
+                  })
+                  .filter(e => !!e)
+              );
+              callback(null, improvisedBuffer);
+              // entry.autodrain();
+            });
+        });
+    }
+  }
+}
+
+const scraper = new Scraper();
+
+scraper.login({
+  user: process.env.API_KEY,
+  password: process.env.SECRET_KEY
+});
+
+scraper.extractData("20190101T00", "20190102T00", function(error, data) {
+  if (error) throw new Error(error);
+  else {
+    fs.writeFile("file.json", data, "utf8", function() {
+      console.log("Process completed.");
+    });
+  }
+});
